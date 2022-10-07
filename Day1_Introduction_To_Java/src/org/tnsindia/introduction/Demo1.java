2c8f80a34777da6e509542213ff95ba4b3c752e0
Deven1919 <86187249+Deven1919@users.noreply.github.com>
2022-10-07 06:56:14 +0530

    First commit

---
+++ Day1_Introduction_To_Java/src/org/tnsindia/introduction/Demo1.java
@@ -0,0 +1,11 @@
+package org.tnsindia.introduction;
+
+public abstract class Demo1 {
+
+	public static void  main (String[]args) {
+		
+		int x= 7;
+		System.out.println("The value of is :"+x);
+		
+	}
+}
---
+++ Day1_Introduction_To_Java/src/org/tnsindia/introduction/Demo2.java
@@ -0,0 +1,10 @@
+package org.tnsindia.introduction;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+	System.out.println("Welcome to M-25 batch!");
+
+	}
+
+}
