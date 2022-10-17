2fde60181c824084ee44fab80f7f4854187d44aa
Hp <Hp@DESKTOP-J098RCP>
2022-10-17 10:31:18 +0530

    First commit

---
+++ Day6_abstract_final_super/src/org/tnsindia/abstraction/Example1.java
@@ -0,0 +1,39 @@
+package org.tnsindia.abstraction;
+
+abstract class A
+{
+	abstract void print();
+}
+
+class B extends A
+{
+	void print()
+	{
+		System.out.println("Welcome to JFS program");
+	}
+}
+public class Example1 {
+
+	public static void main(String[] args) {
+		B obj=new B();
+		obj.print();
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Day6_abstract_final_super/src/org/tnsindia/abstraction/Example2.java
@@ -0,0 +1,13 @@
+package org.tnsindia.abstraction;
+
+public class Example2 {
+
+	public static void main(String[] args) 
+	{
+		final int a=50;
+		//a=14;
+		System.out.println(a);
+
+	}
+
+}
